0800ac36679cdd83a42ed6b3fbfea52d2a7eeeab
osualaFC <fredrick.osuala@decagon.dev>
2020-11-25 21:44:46 +0100

    setup payment gateway

--- app/src/main/java/com/example/shawarmahub/utils/Constants.kt
+++ app/src/main/java/com/example/shawarmahub/utils/Constants.kt
@@ -4,5 +4,8 @@ class Constants {
 
     companion object{
         const val BASE_URL = "http://cashierapi.operapay.com/api/v3/"
+        const val PUBLIC_KEY= "OPAYPUB16059686089660.7070667649464136"
+        const val AES_KEY="JUmQYIbd577tsL4E"
+        const val ID = "256620112118041"
     }
 }
